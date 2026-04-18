3e0fde99ee645ee143200e8fa5f1d4896b2541b6
affan4640 <affanridak@gmail.com>
2026-04-18 23:35:00 +0700

    first

---
+++ resources/js/Pages/landing.jsx
@@ -0,0 +1,10 @@
+export default function landing() {
+  return (
+    <div>
+      <h1>Welcome Landing Page</h1>
+      <a href="/login">Login</a> <br />
+      <a href="/register">Register</a> <br />
+      <a href="/auth/google">Login with Google</a>
+    </div>
+  );
+}
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+Route::get('/', function () {
+    return Inertia::render('landing');
+});
+
+Route::get('/auth/google', function () {
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('/auth/google/callback', function () {
+    $googleUser = Socialite::driver('google')->user();
+
+    $user = User::updateOrCreate(
+        ['email' => $googleUser->getEmail()],
+        ['name' => $googleUser->getName()]
+    );
+
+    Auth::login($user);
+
+    return redirect('/dashboard');
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
